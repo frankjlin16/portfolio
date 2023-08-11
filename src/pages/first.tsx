aff9854c3b1cf100b167a4588d83a7c216f26355
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-08-11 05:12:39 -0700

    feat: add first page

---
+++ src/pages/first.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function First() {
+  return (
+    <>
+      <Link href="/">
+        <h3>Back to home</h3>
+      </Link>
+      <h1>First Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, voluptate, quibusdam, quia voluptas voluptatem
+        exercitationem quos dolorum quod quae doloremque. Quisquam, quia
+        voluptas. Quisquam, quia voluptas. Quisquam, quia voluptas.
+      </p>
+    </>
+  );
+}
