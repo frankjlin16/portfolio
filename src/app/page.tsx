347142860f276a132dd4fe4a545534f64752bed8
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-08-11 09:03:33 -0700

    feat: add homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Index() {
+  return (
+    <>
+      <h1>Hi, I&rsquo;m Frank!</h1>
+      <h2>Let me tell you a little about myself...</h2>
+    </>
+  );
+}
