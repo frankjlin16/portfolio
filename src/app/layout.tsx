c70f7a32c073e7f7c8cd9b2a62f3f55d535bcdd0
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-08-11 14:03:26 -0700

    feat: update page metadata and add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "Frank J Lin",
   description: "Frank Lin's portfolio",
 };
 
