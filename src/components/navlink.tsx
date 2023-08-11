e26aca7d5c5269c0360704699e34cc10d0fa8f34
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-08-11 15:00:53 -0700

    feat: update navlink

--- src/components/navlink.tsx
+++ src/components/navlink.tsx
@@ -10,11 +10,11 @@ export default function Navlink({ href, text }: NavlinkProps) {
     <div className="group">
       <Link
         href={href}
-        className="text-xl text-black group-active:text-gray-700 font-normal"
+        className="text-xl text-black group-active:text-gray-500 font-normal"
       >
         {text}
       </Link>
-      <div className="h-[0px] w-5 group-hover:w-full border border-black  group-active:border-gray-700"></div>
+      <div className="h-[0px] w-5 group-hover:w-full border border-black  group-active:border-gray-500"></div>
     </div>
   );
 }
