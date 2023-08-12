25086ebbc61b0c9ff2c09c950044ae4d48029790
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-08-12 00:26:08 -0700

    feat: add projects section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Navlink from "./navlink";
 export default function Navbar() {
   return (
     <nav className="flex gap-10">
-      <Navlink href="#" text="Project" />
+      <Navlink href="#projects" text="Project" />
       <Navlink href="#" text="Contact" />
     </nav>
   );
