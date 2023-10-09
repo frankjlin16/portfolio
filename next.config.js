f9f62d7839cd5cdcffa8ed964f22cd52dbe2f146
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-10-09 01:19:19 -0700

    feat: update image

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**.googleusercontent.com',
-            }
-        ],
-    },
 }
 
 module.exports = nextConfig
