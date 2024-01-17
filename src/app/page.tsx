180d0499668ac4bbdbfa97b79623cd57e38d1763
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2024-01-17 16:54:34 -0700

    feat: update project link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ const projects = [
     seamless user experience.`,
     image:
       "https://images.unsplash.com/photo-1530435460869-d13625c69bbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-    link: "#", //TODO: Add link
+    link: "https://bjmajestic.com/",
   },
 ];
 
